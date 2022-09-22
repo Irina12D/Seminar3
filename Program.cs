7dca76639e0afc7de44a74d39b297c340a53cb25
Irina <countess.all@yandex.ru>
2022-09-22 22:10:00 +0300

    вторая задача

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@
     23432 -> да
 */
 
+/*
 // приведу два варианта решения: оперирование строкой и оперирование числом
 
 Console.Write("Введите пятизначное число: ");
@@ -36,5 +37,24 @@ if(number / 10000 == number % 10 && number / 1000 % 10 == number / 10 % 10)
      Console.WriteLine("да");
 else 
      Console.WriteLine("нет");     
+*/
 
+
+/*
+Задача 21
+Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+    A (3,6,8); B (2,1,-7), -> 15.84
+    A (7,-5, 0); B (1,-1,9) -> 11.53
+*/
+
+Console.Write("Введите координаты x,y,z точки А через пробел: ");
+string []s = Console.ReadLine().Split();
+int ax = int.Parse(s[0]), ay = int.Parse(s[1]), az = int.Parse(s[2]);
+
+Console.Write("Введите координаты x,y,z точки В через пробел: ");
+s = Console.ReadLine().Split();
+int bx = int.Parse(s[0]), by = int.Parse(s[1]), bz = int.Parse(s[2]);
+
+double r = Math.Sqrt((ax-bx)*(ax-bx) + (ay-by)*(ay-by) + (az-bz)*(az-bz));
+Console.WriteLine($"рассточние между точками равно {r}");
 
